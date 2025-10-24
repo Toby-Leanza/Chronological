7b760ee12814dc96ec359fd2d0cd20d268153f28
Julian <julivaccari48@gmail.com>
2025-10-23 23:22:56 -0300

    implementacion de reinicio de nivel
    Al presionar la O el nivel o escena se reinicia.

---
+++ Assets/Scripts/Time/RestartScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
