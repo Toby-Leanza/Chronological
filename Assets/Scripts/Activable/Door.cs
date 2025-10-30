7bdd10c6718eadfd51e81be1f42e0c37f632a1c1
Toby-Leanza <leanzatobias@outlook.com>
2025-10-30 01:02:10 -0300

    Mecanismo de teletransporte y arreglo de la puerta

--- Assets/Scripts/Activable/Door.cs
+++ Assets/Scripts/Activable/Door.cs
@@ -1,7 +1,20 @@
+using UnityEngine;
+
 public class Door : Activable
 {
+    public GameObject doorOpen;    // Referencia al modelo abierto
+    public GameObject doorClosed;  // Referencia al modelo cerrado
+
+    private void Start()
+    {
+        // Estado inicial
+        doorOpen.SetActive(false);
+        doorClosed.SetActive(true);
+    }
+
     public override void SetActiveState(bool state)
     {
-        gameObject.SetActive(!state);
+        doorOpen.SetActive(state);
+        doorClosed.SetActive(!state);
     }
 }
